2ec395b3a00a852c4a54bd6bc5a3299aa26a197f
Pamela Frieze <pfrieze@gmail.com>
2017-12-18 05:04:31 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 require_relative '../kickstarter_scraper'
 
 def create_project_hash
-html = File.read('fixtures/kickstarter.html')
- 
-kickstarter = Nokogiri::HTML(html)
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
+end
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # config here
