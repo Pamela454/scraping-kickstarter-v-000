cbdb26567fadc372123ab0937308aaeb74991161
Pamela Frieze <pfrieze@gmail.com>
2017-12-18 05:19:55 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,15 @@
 require_relative '../kickstarter_scraper'
 require 'pry'
 
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  # config here
+end
+
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-end
-# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-RSpec.configure do |config|
-  # config here
 end
 
 binding.pry
