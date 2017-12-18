7a7cc7966d4f577f5d4158c509107dc599d0752b
Pamela Frieze <pfrieze@gmail.com>
2017-12-18 05:34:21 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,8 @@ end
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   # projects: kickstarter.css("li.project.grid_4")
+  # projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
   kickstarter = Nokogiri::HTML(html)
 
   binding.pry
