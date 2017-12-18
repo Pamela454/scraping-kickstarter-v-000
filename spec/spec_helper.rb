e59dfa85f58407c588ffc431a1008e40b5d27271
Pamela Frieze <pfrieze@gmail.com>
2017-12-18 05:21:59 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
-require_relative '../kickstarter_scraper'
 require 'pry'
+
+require_relative '../kickstarter_scraper'
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
